f61a97a11e1ffdc022b13e1cea86db985590a804
calvindany <calvindany1102@gmail.com>
2023-04-26 12:11:29 +0700

    Adding base look for barang page

--- controller/AdminController.js
+++ controller/AdminController.js
@@ -4,10 +4,13 @@ exports.getDashboard = (req, res, next) => {
     isAdmin = true;
   }
   res.render("admin/dashboard", {
+    route: "/dashboard",
     isAdmin: isAdmin,
   });
 };
 
 exports.getBarang = (req, res, next) => {
-  res.render("admin/barang");
+  res.render("admin/barang", {
+    route: "/barang",
+  });
 };
